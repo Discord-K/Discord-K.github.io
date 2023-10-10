e218d14d57b4858c71f86ff77eac5c7ecf56cda5
Discord <2874849065@qq.com>
2023-10-10 12:06:18 +0800

    feat: 基本完成

---
+++ src/js/animation.js
@@ -0,0 +1,68 @@
+window.onload = function (){
+    const body = document.body;
+    if (window.scrollY === 0)body.style.overflow = 'hidden';
+    const cover = document.getElementById('cover');
+    cover.style.opacity = '1';
+    const headline = document.getElementById('headline');
+    headline.style.opacity = '0';
+    let top = 100;
+    // 首图标题浮现动效
+    cover.onwheel = function (event){
+        const deltaY = event.deltaY;
+        const opacity = cover.style.opacity;
+
+        if(deltaY > 0 && opacity > 0.5){
+            cover.style.opacity = `${opacity - 0.25}`;
+            headline.style.opacity = `${parseFloat(headline.style.opacity) + 0.5}`;
+            top = top - 25;
+            headline.style.top = `${top}%`;
+        }else if(deltaY < 0 && opacity < 1 && opacity > 0){
+            cover.style.opacity = `${parseFloat(opacity)+0.25}`;
+            headline.style.opacity = `${parseFloat(headline.style.opacity) - 0.5}`;
+            top = top + 25;
+            headline.style.top = `${top}%`;
+        }
+        if(cover.style.opacity <= 0.5) body.style.overflow = 'auto';
+        if(cover.style.opacity > 0.75) body.style.overflow = 'hidden';
+    }
+
+    // 滑动图层动效
+    const slides = document.getElementsByClassName('slide');
+    // slides.forEach()console.log(slide)
+    for (let i = 0; i < slides.length; i++) {
+        const slide = slides[i];
+        const height = slide.clientHeight + 100;
+        slide.onscroll = function () {
+            const className = slide.className.split(' ')[1];
+            const children = (slide.children.length - 1) / 2;
+            if ( i === 1 ) {
+                if (slide.scrollTop > 0 && slide.scrollTop <= 0.05 * height) slide.style.backgroundImage = `url('img/${className}1.png')`;
+                if (slide.scrollTop > 0.2 * height && slide.scrollTop <= 1.1 * height) slide.style.backgroundImage = `url('img/${className}2.png')`;
+                for (let j = 2; j < children + 1; j++) {
+                    const imgName = `url('img/${className}${j + 1}.png')`;
+                    if (slide.scrollTop > (j-1) * height && slide.scrollTop <= j * 1.1 * height) slide.style.backgroundImage = imgName;
+                }
+            }else for (let j = 0; j < children; j++) {
+                const imgName = `url('img/${className}${j + 1}.png')`;
+                if (slide.scrollTop > j * 0.8 * height && slide.scrollTop <= (j + 1) * 1.2 * height) slide.style.backgroundImage = imgName;
+            }
+        }
+    }
+}
+
+window.onscroll = function (){
+    const top = [];
+    for (let i = 0; i < 6; i++) {
+        const anchor = document.getElementById('anchor' + i).getBoundingClientRect();
+        top.push(anchor.top);
+    }
+    const order = top.filter(d=>(d<10)).length;
+    const anchor = document.getElementById('nav' + order);
+    for (let i = 1; i < 7; i++) {
+        const nav = document.getElementById('nav' + i);
+        nav.style.borderBottom = '0';
+        nav.style.fontWeight = '400';
+    }
+    anchor.style.borderBottom = '#243b6d 4px solid';
+    anchor.style.fontWeight = '900';
+}
